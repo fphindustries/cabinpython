86da3365cef3a47fbf0270ef8d483bd69a7b1bdf
C Kent <cpkent@gmail.com>
2017-03-14 11:35:36 -0700

    Changed POST code

--- cabinpy.py
+++ cabinpy.py
@@ -1,148 +1,50 @@
 #!/usr/bin/env python
-import iothub_client
 import sys
+import requests
 
-from iothub_client import IoTHubClient, IoTHubClientError, IoTHubTransportProvider, IoTHubClientResult
-from iothub_client import IoTHubMessage, IoTHubMessageDispositionResult, IoTHubError, DeviceMethodReturnValue
-from iothub_client_args import get_iothub_opt, OptionError
 from Adafruit_SHT31 import *
+from datetime import datetime
 #logging.basicConfig(filename='example.log',level=logging.DEBUG)
+from ina219 import INA219
+from ina219 import DeviceRangeError
 
-# messageTimeout - the maximum time in milliseconds until a message times out.
-# The timeout period starts at IoTHubClient.send_event_async.
-# By default, messages do not expire.
-MESSAGE_TIMEOUT = 10000
+SHUNT_OHMS = 0.1
 
-RECEIVE_CONTEXT = 0
-TWIN_CONTEXT = 0
-METHOD_CONTEXT = 0
-SEND_REPORTED_STATE_CONTEXT = 0
 
-# global counters
-RECEIVE_CALLBACKS = 0
-SEND_CALLBACKS = 0
-BLOB_CALLBACKS = 0
-TWIN_CALLBACKS = 0
-SEND_REPORTED_STATE_CALLBACKS = 0
-METHOD_CALLBACKS = 0
+def read_power():
+    ina = INA219(SHUNT_OHMS)
+    ina.configure()
+    return ina.voltage(), ina.current(), ina.power()
 
-# chose HTTP, AMQP or MQTT as transport protocol
-PROTOCOL = IoTHubTransportProvider.MQTT
-
-# String containing Hostname, Device Id & Device Key in the format:
-# "HostName=<host_name>;DeviceId=<device_id>;SharedAccessKey=<device_key>"
-CONNECTION_STRING = "[Device Connection String]"
-
-def read_sensor():
+def read_env():
     sensor = SHT31(address = 0x44)
     #sensor = SHT31(address = 0x45)
 
     degrees, humidity = sensor.read_temperature_humidity()
     degrees = (9.0/5.0) * degrees + 32
 
-    return '{{"temp": {0:0.3f},"hum": {1:0.2f}}}'.format(degrees, humidity)
+    return degrees, humidity
 
-def receive_message_callback(message, counter):
-    global RECEIVE_CALLBACKS
-    message_buffer = message.get_bytearray()
-    size = len(message_buffer)
-    print ( "Received Message [%d]:" % counter )
-    print ( "    Data: <<<%s>>> & Size=%d" % (message_buffer[:size].decode('utf-8'), size) )
-    map_properties = message.properties()
-    key_value_pair = map_properties.get_internals()
-    print ( "    Properties: %s" % key_value_pair )
-    counter += 1
-    RECEIVE_CALLBACKS += 1
-    print ( "    Total calls received: %d" % RECEIVE_CALLBACKS )
-    return IoTHubMessageDispositionResult.ACCEPTED
-
-
-def send_confirmation_callback(message, result, user_context):
-    global SEND_CALLBACKS
-    print ( "Confirmation[%d] received for message with result = %s" % (user_context, result) )
-    map_properties = message.properties()
-    print ( "    message_id: %s" % message.message_id )
-    print ( "    correlation_id: %s" % message.correlation_id )
-    key_value_pair = map_properties.get_internals()
-    print ( "    Properties: %s" % key_value_pair )
-    SEND_CALLBACKS += 1
-    print ( "    Total calls confirmed: %d" % SEND_CALLBACKS )
-
-
-def device_twin_callback(update_state, payload, user_context):
-    global TWIN_CALLBACKS
-    print ( "\nTwin callback called with:\nupdateStatus = %s\npayload = %s\ncontext = %s" % (update_state, payload, user_context) )
-    TWIN_CALLBACKS += 1
-    print ( "Total calls confirmed: %d\n" % TWIN_CALLBACKS )
-
-def send_reported_state_callback(status_code, user_context):
-    global SEND_REPORTED_STATE_CALLBACKS
-    print ( "Confirmation for reported state received with:\nstatus_code = [%d]\ncontext = %s" % (status_code, user_context) )
-    SEND_REPORTED_STATE_CALLBACKS += 1
-    print ( "    Total calls confirmed: %d" % SEND_REPORTED_STATE_CALLBACKS )
-
-
-def device_method_callback(method_name, payload, user_context):
-    global METHOD_CALLBACKS
-    print ( "\nMethod callback called with:\nmethodName = %s\npayload = %s\ncontext = %s" % (method_name, payload, user_context) )
-    METHOD_CALLBACKS += 1
-    print ( "Total calls confirmed: %d\n" % METHOD_CALLBACKS )
-    device_method_return_value = DeviceMethodReturnValue()
-    device_method_return_value.response = "{ \"Response\": \"This is the response from the device\" }"
-    device_method_return_value.status = 200
-    return device_method_return_value
-
-
-def blob_upload_conf_callback(result, user_context):
-    global BLOB_CALLBACKS
-    print ( "Blob upload confirmation[%d] received for message with result = %s" % (user_context, result) )
-    BLOB_CALLBACKS += 1
-    print ( "    Total calls confirmed: %d" % BLOB_CALLBACKS )
-
-def iothub_client_init():
-    # prepare iothub client
-    client = IoTHubClient(CONNECTION_STRING, PROTOCOL)
-    if client.protocol == IoTHubTransportProvider.HTTP:
-        client.set_option("timeout", TIMEOUT)
-        client.set_option("MinimumPollingTime", MINIMUM_POLLING_TIME)
-    # set the time until a message times out
-    client.set_option("messageTimeout", MESSAGE_TIMEOUT)
-    # some embedded platforms need certificate information
-    #set_certificates(client)
-    # to enable MQTT logging set to 1
-    if client.protocol == IoTHubTransportProvider.MQTT:
-        client.set_option("logtrace", 0)
-    client.set_message_callback(
-        receive_message_callback, RECEIVE_CONTEXT)
-    if client.protocol == IoTHubTransportProvider.MQTT:
-        client.set_device_twin_callback(
-            device_twin_callback, TWIN_CONTEXT)
-        client.set_device_method_callback(
-            device_method_callback, METHOD_CONTEXT)
-    return client
+def create_sample(sensorName, reading):
+    return {'SensorName':sensorName, 'SampleTime':datetime.now().isoformat(), 'Value':reading}
 
 def main():  
     try:
-        client = iothub_client_init()
-
-        if client.protocol == IoTHubTransportProvider.MQTT:
-            print ( "IoTHubClient is reporting state" )
-            reported_state = "{\"newState\":\"standBy\"}"
-            client.send_reported_state(reported_state, len(reported_state), send_reported_state_callback, SEND_REPORTED_STATE_CONTEXT)
-
         while True:
-            message = IoTHubMessage(read_sensor())          
-            client.send_event_async(message, send_confirmation_callback, 0)
-            print ( "IoTHubClient.send_event_async accepted message for transmission to IoT Hub.")
+            degrees, humidity =read_env()
+            v,a,w = read_power()
+            readings = []
+            readings.append(create_sample('InsideTemp', degrees))
+            readings.append(create_sample('InsideHum', humidity))
+            readings.append(create_sample('PiVolts', v))
+            readings.append(create_sample('PimAmps', a))
+            readings.append(create_sample('PimWatts', w))
 
-            # Wait for Commands or exit
-            print ( "IoTHubClient waiting for commands, press Ctrl-C to exit" )
-
+            print(readings)
+            r=requests.post('http://cabinpi.azurewebsites.net/api/telemetry/', json = readings)
+            print(r)
             time.sleep(30)
 
-    except IoTHubError as iothub_error:
-        print ( "Unexpected error %s from IoTHub" % iothub_error )
-        return
     except KeyboardInterrupt:
         print ( "IoTHubClient sample stopped" )
 
@@ -154,17 +56,5 @@ def usage():
 
 if __name__ == '__main__':
     print ( "\nPython %s" % sys.version )
-    print ( "IoT Hub for Python SDK Version: %s" % iothub_client.__version__ )
-
-    try:
-        (CONNECTION_STRING, PROTOCOL) = get_iothub_opt(sys.argv[1:], CONNECTION_STRING, PROTOCOL)
-    except OptionError as option_error:
-        print ( option_error )
-        usage()
-        sys.exit(1)
-
-    print ( "Starting the IoT Hub Python sample..." )
-    print ( "    Protocol %s" % PROTOCOL )
-    print ( "    Connection string=%s" % CONNECTION_STRING )
 
     main()
